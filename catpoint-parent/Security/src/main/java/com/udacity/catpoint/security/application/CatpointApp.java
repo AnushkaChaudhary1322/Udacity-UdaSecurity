b13dd53f215c7e21344ccbf61ac83942b9ef228f
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2026-03-08 15:55:21 +0530

    security -> application updated

---
+++ catpoint-parent/Security/src/main/java/com/udacity/catpoint/security/application/CatpointApp.java
@@ -0,0 +1,11 @@
+package com.udacity.catpoint.security.application;
+
+/**
+ * Main class to launch application
+ */
+public class CatpointApp {
+    public static void main(String[] args) {
+        CatpointGui gui = new CatpointGui();
+        gui.setVisible(true);
+    }
+}
---
+++ catpoint-parent/Security/src/main/java/com/udacity/catpoint/security/application/ControlPanel.java
@@ -0,0 +1,42 @@
+package com.udacity.catpoint.security.application;
+
+import com.udacity.catpoint.security.data.ArmingStatus;
+import com.udacity.catpoint.security.service.SecurityService;
+import com.udacity.catpoint.security.service.StyleService;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * JPanel containing the buttons to manipulate arming status of the system.
+ */
+public class ControlPanel extends JPanel {
+
+    private SecurityService securityService;
+    private Map<ArmingStatus, JButton> buttonMap;
+
+
+    public ControlPanel(SecurityService securityService) {
+        super();
+        setLayout(new MigLayout());
+        this.securityService = securityService;
+        JLabel panelLabel = new JLabel("System Control");
+        panelLabel.setFont(StyleService.HEADING_FONT);
+        add(panelLabel, "span 3, wrap");
+        buttonMap = Arrays.stream(ArmingStatus.values())
+                .collect(Collectors.toMap(status -> status, status -> new JButton(status.getDescription())));
+        buttonMap.forEach((k, v) -> {
+            v.addActionListener(e -> {
+                securityService.setArmingStatus(k);
+                buttonMap.forEach((status, button) -> button.setBackground(status == k ? status.getColor() : null));
+            });
+        });
+        Arrays.stream(ArmingStatus.values()).forEach(status -> add(buttonMap.get(status)));
+        ArmingStatus currentStatus = securityService.getArmingStatus();
+        buttonMap.get(currentStatus).setBackground(currentStatus.getColor());
+
+
+    }
+}
